5003d94c4ad8cb62b39528d0be5c209df712e914
plateauu <plateauu@o2.pl>
2020-12-04 22:37:59 +0100

    added parser implementation

--- src/main/java/dev/insidemind/advent/day3/App.java
+++ src/main/java/dev/insidemind/advent/day3/App.java
@@ -3,8 +3,12 @@ package dev.insidemind.advent.day3;
 import dev.insidemind.advent.LinesReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Day 3 Advent od code 2020
@@ -22,8 +26,43 @@ class App {
 
     }
 
-
     static class TrajectoryParser {
+        private final List<String> lines;
 
+        TrajectoryParser(List<String> lines) {
+            this.lines = lines;
+        }
+
+        private Map<Integer, List<Field>> parse() {
+            var counter = new AtomicInteger(0);
+            return lines.stream()
+                        .collect(Collectors.toMap(
+                                l -> counter.incrementAndGet(), l -> parse(l, counter.get())
+                        ));
+        }
+
+        private static List<Field> parse(String line, int lineNumber) {
+            var chars = line.toCharArray();
+            var result = new ArrayList<Field>();
+            for (int i = 0; i < chars.length; i++) {
+                result.add(new Field(lineNumber, i + 1, chars[i]));
+            }
+            return result;
+        }
+
+        enum FieldType {
+            TREE('.'), OPEN_SQUARE('#');
+            char symbol;
+
+            FieldType(char symbol) {
+                this.symbol = symbol;
+            }
+        }
+
+        static record Field(int line, int index, FieldType type) {
+            Field(int line, int index, char field) {
+                this(line, index, FieldType.valueOf(new String(new char[]{field})));
+            }
+        }
     }
 }
