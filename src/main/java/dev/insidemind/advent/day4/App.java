ed0c51c3f9d9378b3666cf6468d7317687d2ded4
plateauu <plateauu@o2.pl>
2020-12-06 22:11:08 +0100

    line merger looks already done

--- src/main/java/dev/insidemind/advent/day4/App.java
+++ src/main/java/dev/insidemind/advent/day4/App.java
@@ -3,8 +3,11 @@ package dev.insidemind.advent.day4;
 import dev.insidemind.advent.LinesReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.regex.Pattern;
 
 /**
  * Day 4 Advent od code 2020
@@ -13,11 +16,37 @@ import java.util.function.Function;
 class App {
     static List<String> lines;
 
+    private static class CredentialMerger {
+        private final List<String> lines;
+
+        CredentialMerger(List<String> lines) {
+            this.lines = lines;
+        }
+
+        List<String> merge() {
+            var result = new LinkedList<String>();
+            Pattern p = Pattern.compile("^\\s*$");
+            var toOneLine = new ArrayList<String>();
+
+            for (var line : lines) {
+                if (!p.matcher(line).matches()) {
+                    toOneLine.add(line);
+                } else {
+                    var joined = String.join(" ", toOneLine);
+                    result.add(joined);
+                    toOneLine.clear();
+                }
+            }
+            return result;
+        }
+    }
+
     static {
-        Path INPUT = Paths.get("src/main/java/dev/insidemind/advent/day3/input.txt");
+        Path INPUT = Paths.get("src/main/java/dev/insidemind/advent/day4/input.txt");
         lines = LinesReader.readAllLines(INPUT, Function.identity());
     }
 
     public static void main(String[] args) {
+        new CredentialMerger(lines);
     }
 }
