78a9a96cc0a86a6e25792d72da99e77560ce58fe
plateauu <plateauu@o2.pl>
2020-12-08 22:37:06 +0100

    Test added and it works for rows. Need to develop columns now

--- src/main/java/dev/insidemind/advent/day5/App.java
+++ src/main/java/dev/insidemind/advent/day5/App.java
@@ -3,11 +3,8 @@ package dev.insidemind.advent.day5;
 import dev.insidemind.advent.LinesReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Function;
-import java.util.regex.Pattern;
 
 /**
  * Day 5 Advent od code 2020
@@ -21,8 +18,55 @@ class App {
         lines = LinesReader.readAllLines(INPUT, Function.identity());
     }
 
+    private static final int from = 127;
 
+    public static void main(String[] args) {
+        new SeatFinder("row");
+    }
 
+    /**
+     * Start by considering the whole range, rows 0 through 127.
+     * F means to take the lower half, keeping rows 0 through 63.
+     * B means to take the upper half, keeping rows 32 through 63.
+     * F means to take the lower half, keeping rows 32 through 47.
+     * B means to take the upper half, keeping rows 40 through 47.
+     * B keeps rows 44 through 47.
+     * F keeps rows 44 through 45.
+     * The final F keeps the lower of the two, row 44.
+     */
+    static class SeatFinder {
+        final String row;
+        final char _F = 'F';
+        final char _B = 'B';
+        final char _A = 'A';
+        final char _R = 'R';
 
+        private final char[] chars;
+
+        SeatFinder(String row) {
+            this.row = row;
+            this.chars = row.toCharArray();
+        }
+
+        public int find() {
+            var start = 0;
+            var end = from;
+            int pivot = 128;
+
+            for (int i = 0; i < chars.length; i++) {
+                pivot = divide(pivot);
+                if (chars[i] == _F) {
+                    end = end - pivot;
+                } else {
+                    start = start + pivot;
+                }
+            }
+            return chars[6]==_F ? start : end;
+        }
+
+        private int divide(int from) {
+            return from >>> 1;
+        }
+    }
 
 }
--- src/test/java/dev/insidemind/advent/day5/AppTest.java
+++ src/test/java/dev/insidemind/advent/day5/AppTest.java
@@ -1,16 +1,61 @@
 package dev.insidemind.advent.day5;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class AppTest {
 
     @Test
     void testName() {
-        System.out.printf("Right shifted >>> : %d%n", 127 >>> 1);
+        var i = 127 >>> 1;
+        System.out.printf("Right shifted >>> : %d%n", i);
+//        System.out.printf("Right shifted >>> : %d%n", 127 - i);
         System.out.printf("Right shifted >>> : %d%n", 127 >>> 1 >>> 1);
         System.out.printf("Right shifted >>> : %d%n", 127 >>> 1 >>> 1 >>> 1);
         System.out.printf("Right shifted >>> : %d%n", 127 >>> 1 >>> 1 >>> 1 >>> 1);
         System.out.printf("Right shifted >>> : %d%n", 127 >>> 1 >>> 1 >>> 1 >>> 1 >>> 1);
         System.out.printf("Right shifted >>> : %d%n", 127 >>> 1 >>> 1 >>> 1 >>> 1 >>> 1 >>> 1);
+    }
+
+    @Test
+    void testName2() {
+        var v = 127;
+        var d = divide(v);
+        System.out.printf("result1: %d%n", v - d);
+        v = v - d;
+        d = divide(d);
+        System.out.printf("result2: %d%n", v - d);
+        d = divide(v);
+        System.out.printf("result3: %d%n", v - d);
+        d = divide(v);
+        System.out.printf("result4: %d%n", v - d);
+        d = divide(v);
+        System.out.printf("result5: %d%n", v - d);
+        d = divide(v);
+        System.out.printf("result6: %d%n", v - d);
+    }
+
+    private int divide(int d) {
+        return d - (d >>> 1);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "FBFBBFFRLR,44",
+            "BFFFBBFRRR,70",
+            "FFFBBBFRRR,14",
+            "BBFFBBFRLL,102",
+    })
+    void shouldProperlyResultToEmptySeat(String input, Integer expected) {
+        /**
+         * FBFBBFFRLR: row 44, column 5
+         * BFFFBBFRRR: row 70, column 7, seat ID 567.",
+         * FFFBBBFRRR: row 14, column 7, seat ID 119.",
+         * BBFFBBFRLL: row 102, column 4, seat ID 820."
+         */
+        var seatFinder = new App.SeatFinder(input);
+        Assertions.assertEquals(expected, seatFinder.find());
     }
 }
