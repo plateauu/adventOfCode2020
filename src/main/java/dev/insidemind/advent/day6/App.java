c8fd3eb081ad94abd27e88d828c365835e8520cf
plateauu <plateauu@o2.pl>
2020-12-13 19:29:01 +0100

    almost done

--- src/main/java/dev/insidemind/advent/day6/App.java
+++ src/main/java/dev/insidemind/advent/day6/App.java
@@ -4,12 +4,15 @@ import dev.insidemind.advent.LinesReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Day 6 Advent od code 2020
@@ -24,11 +27,12 @@ class App {
     }
 
     public static void main(String[] args) {
-        partOne();
+        var groups = new CustomGroupMerger(lines).merge();
+        partOne(groups);
+        partTwo(groups);
     }
 
-    private static void partOne() {
-        var groups = new CustomGroupMerger(lines).merge();
+    private static void partOne(List<CustomGroup> groups) {
         long start = System.currentTimeMillis();
         var result = sumYesGroups(groups);
         System.out.printf("Sum of all yes group is: %d%n", result);
@@ -37,10 +41,25 @@ class App {
         System.out.printf("Time spend: %dms%n", time);
     }
 
+    private static void partTwo(List<CustomGroup> groups) {
+        long start = System.currentTimeMillis();
+        var result = sumEveryoneAnsweredYesGroups(groups);
+        System.out.printf("Sum of all answer everyone say yes is: %d%n", result);
+        long stop = System.currentTimeMillis();
+        var time = stop - start;
+        System.out.printf("Time spend: %dms%n", time);
+    }
     record GroupToYesAnswers(CustomGroup g, Set<Character> yesAnswers) {
         public int count() {
             return yesAnswers().size();
         }
+    }
+
+    private static long sumEveryoneAnsweredYesGroups(List<CustomGroup> groups) {
+        return groups.stream()
+                     .map(CustomGroup::countEveryoneAnswered)
+                     .mapToLong(value -> value)
+                     .sum();
     }
 
     private static int sumYesGroups(List<CustomGroup> groups) {
@@ -90,10 +109,31 @@ class App {
 
     record Person(String answers){}
 
-
     record CustomGroup(List<Person> persons) {
         String joinAnswers() {
             return persons.stream().map(Person::answers).collect(Collectors.joining());
+        }
+
+        long countEveryoneAnswered() {
+            Map<Person, String> p = persons.stream()
+                                           .collect(Collectors.toMap(Function.identity(), person -> person.answers));
+
+            var result = new HashMap<Character, List<Person>>();
+            p.forEach((person, answers) -> {
+                for (var answer : answers.toCharArray()) {
+                    result.compute(answer, (c, r) -> {
+                        if (r == null)
+                            r = new ArrayList<>();
+                        r.add(person);
+                        return r;
+                    });
+                }
+            });
+
+            return result.values()
+                         .stream()
+                         .filter(l -> l.size() == persons.size())
+                         .count();
         }
 
     }
