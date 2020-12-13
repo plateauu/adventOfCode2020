35cb923adf13074111dc0dbfd05f6b33d6bc5eac
plateauu <plateauu@o2.pl>
2020-12-13 18:25:50 +0100

    first step is to high

--- src/main/java/dev/insidemind/advent/day6/App.java
+++ src/main/java/dev/insidemind/advent/day6/App.java
@@ -3,8 +3,12 @@ package dev.insidemind.advent.day6;
 import dev.insidemind.advent.LinesReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Function;
+import java.util.regex.Pattern;
 
 /**
  * Day 6 Advent od code 2020
@@ -13,12 +17,73 @@ import java.util.function.Function;
 class App {
     static List<String> lines;
 
-    public static void main(String[] args) {
-
-    }
-
     static {
         Path INPUT = Paths.get("src/main/java/dev/insidemind/advent/day6/input.txt");
         lines = LinesReader.readAllLines(INPUT, Function.identity());
+    }
+
+    public static void main(String[] args) {
+        var groups = new CustomGroupMerger(lines).merge();
+        long start = System.currentTimeMillis();
+        var result = sumYesGroups(groups);
+        System.out.printf("Sum of all yes group is: %d%n", result);
+        long stop = System.currentTimeMillis();
+        var time = stop - start;
+        System.out.printf("Time spend: %dms%n", time);
+    }
+
+    record GroupToYesAnswers(CustomGroup g, Set<Character> yesAnswers) {
+        public int count() {
+            return yesAnswers().size();
+        }
+    }
+
+    private static int sumYesGroups(List<CustomGroup> groups) {
+        return groups.stream()
+                     .map(group -> {
+                         Set<Character> c = new HashSet<>();
+                         var chars = group.oneLine().toCharArray();
+                         for (char aChar : chars) {
+                             c.add(aChar);
+                         }
+                         return new GroupToYesAnswers(group, c);
+                     })
+                     .map(GroupToYesAnswers::count)
+                     .mapToInt(value -> value)
+                     .sum();
+    }
+
+    static class CustomGroupMerger {
+        private final List<String> lines;
+        private final List<String> toOneLine = new ArrayList<>();
+        private final List<CustomGroup> result = new ArrayList<>();
+
+        public CustomGroupMerger(List<String> lines) {
+            this.lines = lines;
+        }
+
+        public List<CustomGroup> merge() {
+            Pattern p = Pattern.compile("^\\s*$");
+
+            for (var line : lines) {
+                if (!p.matcher(line).matches()) {
+                    toOneLine.add(line);
+                } else {
+                    join();
+                }
+            }
+
+            join();
+            return result;
+        }
+
+        private void join() {
+            var joined = String.join(" ", toOneLine);
+            result.add(new CustomGroup(joined));
+            toOneLine.clear();
+        }
+    }
+
+    record CustomGroup(String oneLine) {
     }
 }
