c7732361e038ebf47d8cf05aa7a5890273a5bd2f
Marcin Michalak <marcin.michalak@decerto.com>
2020-12-26 13:02:10 +0100

    Day 7 started

---
+++ src/test/java/dev/insidemind/advent/day7/AppTest.java
@@ -0,0 +1,14 @@
+package dev.insidemind.advent.day7;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void testName() {
+
+    }
+
+}
