df6590767e5c1470399e1a8359b08acead611348
Marcin Michalak <marcin.michalak@decerto.com>
2020-12-26 22:52:40 +0100

    parser is ready

---
+++ src/main/java/dev/insidemind/advent/day7/App.java
@@ -0,0 +1,52 @@
+package dev.insidemind.advent.day7;
+
+import dev.insidemind.advent.LinesReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Day 7 Advent od code 2020
+ * https://adventofcode.com/2020/day/7
+ */
+class App {
+    static List<String> lines;
+
+    public static void main(String[] args) {
+
+    }
+
+    static class BagParser {
+        public static final Pattern PATTERN = Pattern.compile(
+            "^(\\w+\\W+\\w+){1} bags contain ((\\d+\\W+\\w+\\W+\\w+) bags?[,|\\.] (\\d+\\W+\\w+\\W+\\w+) bags?[,|\\.]" +
+                " (\\d+\\W+\\w+\\W+\\w+) bags?[,|\\.] (\\d+\\W+\\w+\\W+\\w+) bags?[,|\\.]|no other bags).$");
+
+        private final List<String> lines;
+
+        public BagParser(List<String> lines) {
+            this.lines = lines;
+        }
+
+        String[] parse(String line) {
+            Matcher matcher = PATTERN.matcher(line);
+            int groupCount = matcher.groupCount();
+            var r = new String[groupCount];
+            if (matcher.matches()) {
+                for (int i = 0; i < groupCount; i++) {
+                    r[i] = matcher.group(i + 1);
+                }
+            }
+            return r;
+        }
+
+    }
+
+    static {
+        Path INPUT = Paths.get("src/main/java/dev/insidemind/advent/day7/input.txt");
+        lines = LinesReader.readAllLines(INPUT, Function.identity());
+    }
+
+}
--- src/test/java/dev/insidemind/advent/day7/AppTest.java
+++ src/test/java/dev/insidemind/advent/day7/AppTest.java
@@ -1,14 +1,52 @@
 package dev.insidemind.advent.day7;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class AppTest {
 
     @Test
-    void testName() {
+    void shouldParseRule() {
+        var input = "dark aqua bags contain 1 dull coral bag," +
+            " 4 shiny coral bags, 3 vibrant crimson bags," +
+            " 2 muted black bags.";
 
+        App.BagParser parser = new App.BagParser(List.of(input));
+
+        String[] parsed = parser.parse(input);
+
+        assertNotNull(parsed);
+        assertNotNull(parsed[0]);
+        assertNotNull(parsed[1]);
+        assertNotNull(parsed[2]);
+        assertNotNull(parsed[3]);
+        assertNotNull(parsed[4]);
+        assertNotNull(parsed[5]);
+
+        assertEquals("dark aqua", parsed[0]);
+        assertEquals("1 dull coral", parsed[2]);
+        assertEquals("4 shiny coral", parsed[3]);
+        assertEquals("3 vibrant crimson", parsed[4]);
+        assertEquals("2 muted black", parsed[5]);
+    }
+
+    @Test
+    void shouldParseRuleWhenNoOtherBags() {
+        var input = "faded blue bags contain no other bags.";
+
+        App.BagParser parser = new App.BagParser(List.of(input));
+
+        String[] parsed = parser.parse(input);
+
+        assertNotNull(parsed);
+        assertNotNull(parsed[0]);
+        assertNotNull(parsed[1]);
+
+        assertEquals("faded blue", parsed[0]);
+        assertEquals("no other bags", parsed[1]);
     }
 
 }
